51483c17ac8102c96be1f39590539364a273f64e
Maxime Paulus <paulus.maxime@gmail.com>
2017-06-11 18:41:29 +0100

    f
    fd

--- DataProcessing/moving_averages.py
+++ DataProcessing/moving_averages.py
@@ -7,6 +7,21 @@ client = MongoClient("mongodb://moinolol:bitcoin3000@darkbird.no-ip.info:27017/p
 db = client.poloniex
 symbol = 'BTC_ETH'
 
+def get_trades_indexes(trades, offset, live=False):
+    '''
+    Returns indexes of trades in offset range for each data point in DataFrame
+    of book data
+    '''
+    def indexes(ts):
+        ts = int(ts)
+        i_0 = trades.timestamp.searchsorted([ts-offset], side='left')[0]
+        if live:
+            i_n = -1
+        else:
+            i_n = trades.timestamp.searchsorted([ts-1], side='right')[0]
+        return (i_0, i_n)
+    return trades.index.map(indexes)
+
 def get_trade_df(symbol, min_ts, max_ts, convert_timestamps=False):
     '''
     Returns a DataFrame of trades for symbol in time range
@@ -18,7 +33,7 @@ def get_trade_df(symbol, min_ts, max_ts, convert_timestamps=False):
     if not trades.empty:
         trades = trades.set_index('_id')
         if convert_timestamps:
-            trades.index = pd.to_datetime(trades.index, unit='s')
+            trades.index = pd.to_datetime(trades.date, format='%Y-%m-%d %H:%M:%S')
     return trades
 
 def get_trades_average(trades):
@@ -31,14 +46,15 @@ def get_trades_average(trades):
         trades_n = trades.iloc[x.indexes[0]:x.indexes[1]]
         if not trades_n.empty:
             return (trades_n.rate*trades_n.amount).sum()/trades_n.amount.sum()
-    return (trades.rate/trades.apply(mean_trades, axis=1)).apply(log).fillna(0)
+    return trades.apply(mean_trades, axis=1)
 
-
-trades = get_trade_df(symbol, 1497114000, 1497138824)
-min_ts = trades.index.min() - (60*60*24*50)
-max_ts = trades.index.max()
+trades = get_trade_df(symbol, 1497114000, 1497138824,False)
+trades['indexes'] = get_trades_indexes(trades, 60*60*24*50)
+#min_ts = trades.index.min() - (60*60*24*50)
+#max_ts = trades.index.max()
 
 start = time()
 stage = time()
-
-get_trades_average(trades)
+#print trades
+print trades.columns.values
+print get_trades_average(trades)
