3e634aa916b63a0ff2a57c3c8092a2315d555b37
Maxime Paulus <paulus.maxime@gmail.com>
2017-06-11 22:26:04 +0100

    aggregated
    aggregated

--- DataProcessing/moving_averages_talib.py
+++ DataProcessing/moving_averages_talib.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import yaml
+import math
 from math import pi
 from tornado.ioloop import IOLoop
-from pymongo import MongoClient
 from talib.abstract import *
 from bokeh.application.handlers import FunctionHandler
 from bokeh.application import Application
@@ -18,11 +18,24 @@ symbol = 'BTC_ETH'
 def modify_doc(doc):
     rq = Requests("poloniex")
     trades = rq.getTrades(symbol, 1497114000, 1497138824)
-    trades['SMA'] = SMA(trades, timeperiod=500)
-    print trades
+    trades.index = pd.to_datetime(trades._id, unit='s')
 
-    trades.index.name = '_id'
-    source = ColumnDataSource(data=trades)
+    trades_agg = pd.DataFrame()
+    trades_agg['_id'] = trades['_id'].groupby(pd.TimeGrouper('5Min')).min()
+    trades_agg['close'] = trades['close'].groupby(pd.TimeGrouper('5Min')).agg(lambda x: x.iloc[-1])
+    trades_agg['high'] = trades['high'].groupby(pd.TimeGrouper('5Min')).max()
+    trades_agg['low'] = trades['low'].groupby(pd.TimeGrouper('5Min')).min()
+    trades_agg['open'] = trades['open'].groupby(pd.TimeGrouper('5Min')).agg(lambda x: x.iloc[0])
+    trades_agg['volume'] = trades['volume'].groupby(pd.TimeGrouper('5Min')).sum()
+
+    print trades_agg
+    trades_agg = trades_agg.set_index('_id')
+
+    #trades['date'] = pd.to_datetime(trades.index, format='%Y-%m-%d %H:%M:%S')
+    trades_agg['SMA'] = SMA(trades_agg, timeperiod=20)
+
+    trades_agg.index.name = '_id'
+    source = ColumnDataSource(data=trades_agg)
     TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
     p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title = "MSFT Candlestick", y_axis_label='Price')
@@ -31,13 +44,13 @@ def modify_doc(doc):
     p.xaxis.major_label_orientation = pi / 4
     p.grid.grid_line_alpha = 0.3
 
-    inc = trades.close > trades.open
-    dec = trades.open > trades.close
-    w = 1
+    inc = trades_agg.close > trades_agg.open
+    dec = trades_agg.open > trades_agg.close
+    w = 150
 
-    p.segment(trades.index, trades.high, trades.index, trades.low, color="black")
-    p.vbar(trades.index[inc], w, trades.open[inc], trades.close[inc], fill_color="#D5E1DD", line_color="black")
-    p.vbar(trades.index[dec], w, trades.open[dec], trades.close[dec], fill_color="#F2583E", line_color="black")
+    p.segment(trades_agg.index, trades_agg.high, trades_agg.index, trades_agg.low, color="black")
+    p.vbar(trades_agg.index[inc], w, trades_agg.open[inc], trades_agg.close[inc], fill_color="#D5E1DD", line_color="black")
+    p.vbar(trades_agg.index[dec], w, trades_agg.open[dec], trades_agg.close[dec], fill_color="#F2583E", line_color="black")
 
 
     doc.add_root(column(p))
@@ -55,7 +68,7 @@ def modify_doc(doc):
                 grid_line_color: white
     """))
 
-db = client.poloniex
+
 io_loop = IOLoop.current()
 
 bokeh_app = Application(FunctionHandler(modify_doc))
